1a77d8b92466867457baac44d4de95ce2a9970ba
Dean McDonnell <mcdonnelldean@outlook.com>
2015-10-08 15:52:08 +0100

    Org tidy including, - updated code to match standard - updated all deps - moved from tap to tape - added linting to precommit as per org guidelines - moved folder structure to match org guidelines - corrected licensing issue - added travis file - updated tests to run with seneca lates

--- example.js
+++ example.js
@@ -1,18 +1,16 @@
+'use strict'
 
-var seneca        = require('seneca')()
-  , entSaveStream = require('./')
-  , pear          = seneca.make('pear')
-  , pearImporter  = entSaveStream(seneca, { name$: 'pear' })
+var seneca = require('seneca')()
+var entSaveStream = require('..')
+var pear = seneca.make('pear')
+var pearImporter = entSaveStream(seneca, { name$: 'pear' })
 
-pearImporter.on('one', function() {
+pearImporter.on('one', function () {
   console.log('element saved')
 })
 
-pearImporter.end({
-  name: 'my pear',
-  price: 200
-}, function() {
-  pear.list$({}, function(err, res) {
-    console.log(res)
+pearImporter.end({name: 'my pear', price: 200}, function () {
+  pear.list$({}, function (err, res) {
+    if (!err) console.log(res)
   })
 })
